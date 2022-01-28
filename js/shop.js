4cb543687eb24508f14d207276de4b11bd009427
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2022-01-28 16:13:36 +0900

    sort

--- js/shop.js
+++ js/shop.js
@@ -1,7 +1,3 @@
-어레이.sort(function (a, b) {
-  return a - b;
-});
-
 var products = [
   { id: 0, price: 70000, title: "Blossom Dress" },
   { id: 1, price: 50000, title: "Springfield Shirt" },
@@ -49,7 +45,12 @@ $("#price-sort-button").click(function () {
 // 정렬
 var 어레이 = [2, 3, 5, 7, 1, 4, 8, 22];
 어레이.sort(); // sort() 문자 정렬
-/* sort()함수 
+
+어레이.sort(function (a, b) {
+  return a - b;
+});
+/* 
+   sort()함수 
    1. a,b는 array안에 데이터들 
    2. +를 return하면 a는 오른쪽 b는 왼쪽
    3. -를 return하면 b는 오른쪽 b는 왼쪽
@@ -57,4 +58,67 @@ var 어레이 = [2, 3, 5, 7, 1, 4, 8, 22];
    내림차순일경우 b-a 
 */
 
-// array자료에 필터를 주고 싶을때
+/* 
+   filter()함수
+   array자료에 필터를 주고 싶을때 return 조건식
+   filter함수는 사용 시 새로운 변수에 선언하고 넣어줘야함
+   filter()는 기존 array자료를 변형 X
+   sort()는 기존 array 자료를 변형 O
+*/
+var 어레이 = [2, 3, 5, 7, 1, 4, 8, 22];
+var 뉴어레이 = 어레이.filter(function (a) {
+  return a < 4;
+});
+
+/* 
+   map()함수
+   array자료에 전부 뭐 해주고 싶으면 map()
+   map함수도 사용 시 새로운 변수에 선언 후 넣어준다. 신문법이래... 
+*/
+var 뉴어레이 = 어레이.map(function (a) {
+  return a * 2;
+});
+
+// 이름순정렬 버튼 기능
+// $("#price-sort-name").click(function () {
+//   products.sort(function (a, b) {
+//     return a.title - b.title;
+//   });
+
+//   $(title).eq(0).html(products[0].title);
+//   $(price).eq(0).html(products[0].price);
+
+//   $(title).eq(1).html(products[1].title);
+//   $(price).eq(1).html(products[1].price);
+
+//   $(title).eq(2).html(products[2].title);
+//   $(price).eq(2).html(products[2].price);
+// });
+
+// 오만원 이하 상품만 보기 버튼
+/*
+const 상품명템플릿 = `<h5>${products[i].title}</h5>`;
+const 상품가격템플릿 = `
+  <p>
+    가격 <span>${products[i].price}</span>
+  </p>`;
+*/
+
+$("#price-fliter").click(function () {
+  var 오만원이하제품 = products.filter(function (a) {
+    return a.price <= 50000;
+  });
+  $(".card-group").html(" ");
+  for (i = 0; i < 오만원이하제품.length; i++) {
+    var 오만원이하제품템플릿 = ` 
+    <div class="card">
+    <img src="https://via.placeholder.com/600" />
+    <div class="card-body">
+      <h5 class="title">${오만원이하제품[i].title}</h5>
+      <p>가격 : <span>${오만원이하제품[i].price}</span></p>
+      <button class="btn btn-danger">주문하기</button>
+    </div>
+  </div>`;
+    $(".card-group").append(오만원이하제품템플릿);
+  }
+});
